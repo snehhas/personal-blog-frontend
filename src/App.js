6e7dd201d4d729bcd87d74e010226d56c562a5e9
Sneha Sunny <snehasunny321@gmail.com>
2024-07-14 23:25:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 
 function App() {
